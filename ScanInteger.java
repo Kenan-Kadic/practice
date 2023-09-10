9239ddfef9f496b43b435ca5b1cf3a520b4e6a7d
Kenan Kadic <ken.kadic@outlook.com>
2023-09-10 14:03:16 -0500

    update class name

--- ScanInteger.java
+++ ScanInteger.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Scaninteger {
+public class ScanInteger {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 

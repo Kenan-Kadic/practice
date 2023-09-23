903ed610ac3de04859a14d3e7a7c4eb4ec9ea6a4
Kenan Kadic <ken.kadic@outlook.com>
2023-09-23 17:06:10 -0500

    initial commit

---
+++ Concat.java
@@ -0,0 +1,8 @@
+public class Concat {
+    public static void main(String[] args){
+        String firstname = "Ken";
+        String lastname = "Smith";
+        String concatonate = (firstname + " " + lastname);
+        System.out.println(concatonate);
+    }
+}

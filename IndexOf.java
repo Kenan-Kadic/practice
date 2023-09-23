1dcbb6be58133c35ed12fbb98f372006e73ab542
Kenan Kadic <ken.kadic@outlook.com>
2023-09-23 16:53:23 -0500

    initial commit

--- IndexOf.java
+++ IndexOf.java
@@ -1,6 +1,6 @@
 public class IndexOf {
     public static void main(String[] args) {
         String str = "this is a string";
-        System.out.println(str.indexOf("is"));
+        System.out.println(str.indexOf("a"));
     }
 }

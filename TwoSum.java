ea8cf0a7929a7c9b7c97bca3dea611cc17f2ee8d
Kenan Kadic <ken.kadic@outlook.com>
2023-09-23 16:51:23 -0500

    additional practice

---
+++ TwoSum.java
@@ -0,0 +1,23 @@
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order. */
+
+import java.util.HashMap;
+import java.util.Map;
+
+class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numToIndex = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (numToIndex.containsKey(complement)) {
+                return new int[]{numToIndex.get(complement), i};
+            }
+            numToIndex.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No solution found");
+    }
+}

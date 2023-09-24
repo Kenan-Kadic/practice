e46633d2ca2d7d9b896040758d3c3f3f19347dc5
Kenan Kadic <ken.kadic@outlook.com>
2023-09-24 15:23:47 -0500

    initial commit

---
+++ MathEquations.java
@@ -0,0 +1,19 @@
+public class MathEquations{
+    public static void main(String[] args){
+
+        // The HIGHEST number
+        System.out.println(Math.max(6,5));
+
+        // The Lowest number
+        System.out.println(Math.min(6,5));
+
+        // The Square Root
+        System.out.println(Math.sqrt(6.5));
+
+        // The absolute positive value
+        System.out.println(Math.abs(-7));
+
+        //a random number between 0 and 1
+        System.out.println(Math.random());
+    }
+}
